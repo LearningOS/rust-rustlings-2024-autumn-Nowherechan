52050ffb8ff308c6fbd36e2b273ad1bece5b9ee8
Nowherechan <zzxy233@gmail.com>
2024-10-10 19:38:39 +0800

    finish clippy, conversions and macros

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,19 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let info: Vec<&str> = s.split(',').collect();
+        if info.len() != 2 || info[0].len() == 0 {
+            return Person::default();
+        }
+        match info[1].parse::<usize>() {
+            Ok(age) => Person {
+                name: info[0].into(),
+                age: age,
+            },
+            Err(_) => Person::default(),
+        }
     }
 }
 
